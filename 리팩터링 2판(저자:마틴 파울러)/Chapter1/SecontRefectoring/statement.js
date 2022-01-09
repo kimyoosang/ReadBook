4a4ee282eaa8e6c9a13cae99c6354cbba939a131
kimyoosang <yoo99481@wemember.kr>
2022-01-10 00:11:47 +0900

    [Update] secondRefactoring

---
+++ 리팩터링 2판(저자:마틴 파울러)/Chapter1/SecontRefectoring/statement.js
@@ -0,0 +1,43 @@
+import PLAYS from '../plays.json'
+import INVOICES from '../invoices.json'
+import createStatementData from './createStatementData'
+
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명: for ${data.customer})\n`;
+  for (let perf of invoice.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석\n)`;
+  }
+  result += `총액: ${usd(totalAmount)}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+}
+
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer}</h1>\n)`;
+  result += "<table>\n";
+  result += "<tr><th>연극<th><th>좌석 수</th><th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}석</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
+function uds(aNumber) {
+  return new Intl.NumberFormat("en-US",
+      {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+      }).format(aNumber / 100);
+}
